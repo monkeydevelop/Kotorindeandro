16b3bb5ea21fcd9af65bf255b8adfe61c802b271
monkeydevelop <monkey@w-field.info>
2022-05-13 13:59:27 +0900

    適当にコーディングしたのち

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import com.example.myapplication.ui.main.MainFragment
+import kotlinx.android.synthetic.main.*
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
